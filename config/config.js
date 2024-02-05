ac59eade629977907ad275248744e9b38bd46e3d
ddosvn123 <114403793+ddosvn123@users.noreply.github.com>
2024-02-06 00:24:31 +0700

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,50 @@
+var config = {
+    name: {
+        hovaten: "Lê Trọng Phúc",
+        avatar: "https://zpsocial-f54-org.zadn.vn/8fe8ea707fc59e9bc7d4.jpg",
+        tieusu: [
+"Hi my name is BYTE NETWORK😍",
+"Hey brother, I'm a Developer.",
+"What about you :)?",
+],
+        website: "dstat.space",
+        tichxanh: "./v4.0.2/img/tich-xanh.png",
+        created_by: "TRONGPHUC",
+        all_rights_reserved_year: "2023"
+    },
+    // top nav, home, download, about, bạn hãy chỉnh theo ý mình
+    nav: {
+        nav1: ["HOME", "#home"],
+        nav2: ["DOWNLOAD", "download.html"],
+        nav3: ["ABOUT", "about.html"]
+    },
+    thongbao: {
+        title: "WELCOME BACK",
+        noidung: "Chào Mừng Bạn Đã Đến Với Website",
+      //  facebook: ["https://facebook.com/"],
+      //  zalo: ["https://zalo.me/"] 
+    },
+    // Link mạng xã hội, `K` tượng chưng cho con số theo dõi là ngàn, `+` tượng chưng cho con số theo dõi là trăm.
+    social: {
+        facebook: ["https://facebook.com/", "507K"],
+        youtube: ["https://youtube.com/", "3K"],
+        discord: ["https://discord.com/", "800+"],
+        instagram: ["https://instagram.com/", "7+"] 
+    },
+    skill: {
+        html_css: "76%",
+        php: "37%",
+        javascript: "65%",
+        python: "22%" 
+    },
+    // Link nhạc .mp3 có thể sử dụng https://catbox.moe/ để upload nhạc, có thể thêm nhiều nhạc bằng cách nhân bản mảng.
+    music: {
+        linkbaihat: [
+        "//thanhdieu.com/files/Dont-Côi-Remake.mp3",
+        "//thanhdieu.com/files/Anh-Đã-Quen-Với-Cô-Đơn.mp3",
+        "//thanhdieu.com/files/Về-Bên-Anh.mp3",
+        "//thanhdieu.com/files/Save-Me.mp3",
+        "//thanhdieu.com/files/td1.mp3",
+        ] 
+    } 
+};
---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+$config = array (
+  'head' => 
+  array (
+    'description' => 'THÔNG TIN CÁ NHÂN BYTE NETWORK',
+    'keywords' => 'dstat, dstat.space',
+    'author' => 'BYTE NETWORK',
+    'shortcut_icon' => 'https://zpsocial-f54-org.zadn.vn/8fe8ea707fc59e9bc7d4.jpg',
+    'title' => 'DSTAT.SPACE &mdash; Profile Home',
+    'title_small' => 'WELCOME TO DSTAT.SPACE',
+    'url' => 'https://dstat.space',
+    'image_review' => 'https://zpsocial-f54-org.zadn.vn/8fe8ea707fc59e9bc7d4.jpg',
+    'anti_f12' => './v4.0.2/js/sfs.js',
+    'anti_ddos' => '',
+  ),
+);
